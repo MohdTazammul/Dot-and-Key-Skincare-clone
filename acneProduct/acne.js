890d9dc76baaf8b7e13cad441bb2192229915a55
kultzz <93389594+kultzz@users.noreply.github.com>
2021-12-15 13:09:39 +0530

    navBar

---
+++ acneProduct/acne.js
@@ -0,0 +1,273 @@
+var data=[
+    {
+        title:"10% NIACINAMIDE SPOT CORRECTOR SERUM",
+        images:["https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-8_360x.jpg?v=1639053781",
+        "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Niacinamide-website-image_360x.jpg?v=1639053781",
+        "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Niacinamide_360x.jpg?v=1639053781",
+        "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/NiacinamideListing_360x.jpg?v=1639053781"
+
+        ],
+        size:["30ml"],
+        description:[
+            "Formulated with 10% Niacinamide that brightens and 1% Zinc that rejuvenates",
+            "A superlight and non-greasy serum",
+            "Fades dark spots and minimises appearance of pores",
+            "Reduces redness and improves skin’s texture"
+        ],
+        perks:[
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit above 1899",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+            "20% Cashback on all orders"
+],
+        category:"Acne",
+       
+    },
+
+    {
+        title:"ACNE DEFENSE GREEN CLAY MASK",
+        images:[
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/acneclay_360x.jpg?v=1638990018",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/image-1_360x.png?v=1638990018",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/green_2_360x.png?v=1638990018",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/GreenClayMask_360x.png?v=1638990018"
+        ],
+        size:["85mg"],
+        description:[
+           "An anti acne clay mask that removes toxins to combat recurring acne and skin damage from pollution.",
+           "Supercharged with matcha tea and tamanu oil, it detoxifies and mattifies your skin in just 10 minutes.",
+           "Eucalyptus and neem oil provide a soothing experience, while French Green Clay draws out deep-dwelling impurities to unclog pores.",
+           "Salicylic acid in the face mask minimises the appearance of acne scars, pimples and blemishes.",
+           "A perfect non comedogenic clay mask for congested, acne prone skin."
+        ],
+        perks:[
+            "Get Silicone Applicator Free with this product",
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+],
+        category:"Acne",
+       
+    },
+
+
+    {
+        title:"Peace Out Acne Duo",
+        images:[
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/7.2_5c7a4a7f-5b10-4337-890f-883175f02832_360x.jpg?v=1639047630",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/7a_360x.jpg?v=1639047630"
+        ],
+        size:["25ml"],
+        description:[
+           "An anti acne clay mask that removes toxins to combat recurring acne and skin damage from pollution.",
+           "Supercharged with matcha tea and tamanu oil, it detoxifies and mattifies your skin in just 10 minutes.",
+           "Eucalyptus and neem oil provide a soothing experience, while French Green Clay draws out deep-dwelling impurities to unclog pores.",
+           "Salicylic acid in the face mask minimises the appearance of acne scars, pimples and blemishes.",
+           "A perfect non comedogenic clay mask for congested, acne prone skin."
+        ],
+        perks:[
+            "Get Silicone Applicator Free with this product",
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+],
+        category:"Acne",
+       
+    },
+
+
+    {
+        title:"Goodbye Acne Skincare Regime",
+        images:[
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/9_7dc2bc7e-3f69-4a0e-97c9-da1d7ec21645_360x.jpg?v=1639047740",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/9.a_360x.jpg?v=1639047738"
+        ],
+        size:["30ml"],
+        description:[
+            "Formulated with 10% Niacinamide that brightens and 1% Zinc that rejuvenates",
+            "A superlight and non-greasy serum",
+            "Fades dark spots and minimises appearance of pores",
+            "Reduces redness and improves skin’s texture"
+        ],
+        perks:[
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit above 1899",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+            "20% Cashback on all orders"
+],
+        category:"Acne",
+       
+    },
+
+
+
+    {
+        title:"5 Steps To Acne Free Skin",
+        images:[
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/acne_360x.jpg?v=1638790100",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/acne2_360x.jpg?v=1638790105",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/acne3_1dc70644-ed43-49ef-b2ab-2dc04908da54_360x.jpg?v=1639028669"
+        ],
+        size:["60ml"],
+        description:[
+           "An anti acne clay mask that removes toxins to combat recurring acne and skin damage from pollution.",
+           "Supercharged with matcha tea and tamanu oil, it detoxifies and mattifies your skin in just 10 minutes.",
+           "Eucalyptus and neem oil provide a soothing experience, while French Green Clay draws out deep-dwelling impurities to unclog pores.",
+           "Salicylic acid in the face mask minimises the appearance of acne scars, pimples and blemishes.",
+           "A perfect non comedogenic clay mask for congested, acne prone skin.",
+           "Curated with skincare products that help fight acne and other related skin problems, it is the best skincare kit for acne-prone, oily skin.",
+           "Deep Pore Cleanser: Infused with lactic acid, malic acid & other soothing natural ingredients, it deeply cleanses & unclogs pores for clear, milky bright skin.",
+           "5% AHA Toner: An exfoliating toner made with natural fruit acids derived from grapeseed & more that helps improve the texture of the skin, making it look & feel smoother",
+           "CICA Night Gel:The night gel powered by calming cica (Centella Asiatica) heals acne, inflammation and acne scars to provide clear, healthy skin with balanced hydration. "
+        ],
+        perks:[
+            "Get Silicone Applicator Free with this product",
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+],
+        category:"Acne",
+       
+    },
+
+
+
+    {
+        title:"Mask Must Haves",
+        images:[
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/3_50f381da-c383-4c66-bab9-494ef5c64166_360x.jpg?v=1639046641",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/3a_1_360x.jpg?v=1639046641"
+        ],
+        size:["60ml"],
+        description:[
+            "Formulated with 10% Niacinamide that brightens and 1% Zinc that rejuvenates",
+            "A superlight and non-greasy serum",
+            "Fades dark spots and minimises appearance of pores",
+            "Reduces redness and improves skin’s texture",
+            "The Vitamin C Sleep Mask works overnight to repair skin damage and dullness while promoting cell turnover for visibly glowing, radiant and even-toned skin.",
+            "It is an ideal skincare kit that also saves your skin from winter dryness and dullness"
+        ],
+        perks:[
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit above 1899",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+            "20% Cashback on all orders"
+],
+        category:"Acne",
+       
+    },
+
+    {
+        title:"Hand & Foot Dream Cream Duo",
+        images:[
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/foot_a9de1f88-5f08-47a8-aa96-1ec9e3d16cc5_360x.jpg?v=1639028789",
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/foot1_360x.jpg?v=1639028789"
+        ],
+        size:["30ml"],
+        description:[
+           "An anti acne clay mask that removes toxins to combat recurring acne and skin damage from pollution.",
+           "Supercharged with matcha tea and tamanu oil, it detoxifies and mattifies your skin in just 10 minutes.",
+           "Eucalyptus and neem oil provide a soothing experience, while French Green Clay draws out deep-dwelling impurities to unclog pores.",
+           "Salicylic acid in the face mask minimises the appearance of acne scars, pimples and blemishes.",
+           "A perfect non comedogenic clay mask for congested, acne prone skin.",
+           "Curated with skincare products that help fight acne and other related skin problems, it is the best skincare kit for acne-prone, oily skin.",
+           "Deep Pore Cleanser: Infused with lactic acid, malic acid & other soothing natural ingredients, it deeply cleanses & unclogs pores for clear, milky bright skin.",
+           "5% AHA Toner: An exfoliating toner made with natural fruit acids derived from grapeseed & more that helps improve the texture of the skin, making it look & feel smoother",
+           "CICA Night Gel:The night gel powered by calming cica (Centella Asiatica) heals acne, inflammation and acne scars to provide clear, healthy skin with balanced hydration. "
+        ],
+        perks:[
+            "Pamper yourself and enjoy moisturized hands and feet with Dot & Key's Hand & Foot Dream Cream Duo. ",
+            "Get Silicone Applicator Free with this product",
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+],
+        category:"Acne",
+       
+    },
+
+    {
+        title:"Acne Busting Kit - 15ml",
+        images:[
+            "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Pollution_Acne_1800x1800.jpg?v=1611576921"
+        ],
+        size:["30ml"],
+        description:[
+           "An anti acne clay mask that removes toxins to combat recurring acne and skin damage from pollution.",
+           "Supercharged with matcha tea and tamanu oil, it detoxifies and mattifies your skin in just 10 minutes.",
+           "Eucalyptus and neem oil provide a soothing experience, while French Green Clay draws out deep-dwelling impurities to unclog pores.",
+           "Salicylic acid in the face mask minimises the appearance of acne scars, pimples and blemishes.",
+           "A perfect non comedogenic clay mask for congested, acne prone skin.",
+           "Curated with skincare products that help fight acne and other related skin problems, it is the best skincare kit for acne-prone, oily skin.",
+           "Deep Pore Cleanser: Infused with lactic acid, malic acid & other soothing natural ingredients, it deeply cleanses & unclogs pores for clear, milky bright skin.",
+           "5% AHA Toner: An exfoliating toner made with natural fruit acids derived from grapeseed & more that helps improve the texture of the skin, making it look & feel smoother",
+           "CICA Night Gel:The night gel powered by calming cica (Centella Asiatica) heals acne, inflammation and acne scars to provide clear, healthy skin with balanced hydration. "
+        ],
+        perks:[
+            "Pamper yourself and enjoy moisturized hands and feet with Dot & Key's Hand & Foot Dream Cream Duo. ",
+            "Get Silicone Applicator Free with this product",
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+],
+        category:"Acne",
+       
+    },
+
+    {
+        title:"CICA CALMING SKIN RENEWING NIGHT GEL",
+        images:[
+           "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-2_14c0fef2-c164-47a4-b19a-f53e52ca44dc_360x.jpg?v=1638973757",
+           "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/cica3_1_360x.jpg?v=1638973757",
+           "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/BACKEDBY_360x.jpg?v=1638973757",
+           "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/selfassessment_360x.jpg?v=1638973757",
+           "https://cdn.shopify.com/s/files/1/0361/8553/8692/products/BESTSUITEDOFR_360x.jpg?v=1638973757"
+        ],
+        size:["85mg"],
+        description:[
+           "An anti acne clay mask that removes toxins to combat recurring acne and skin damage from pollution.",
+           "Supercharged with matcha tea and tamanu oil, it detoxifies and mattifies your skin in just 10 minutes.",
+           "Eucalyptus and neem oil provide a soothing experience, while French Green Clay draws out deep-dwelling impurities to unclog pores.",
+           "Salicylic acid in the face mask minimises the appearance of acne scars, pimples and blemishes.",
+           "A perfect non comedogenic clay mask for congested, acne prone skin."
+        ],
+        perks:[
+            "Get Silicone Applicator Free with this product",
+            "FREE Hand Foot duo on orders above 899",
+            "Pick your FREE 5-in-1 kit",
+            "FREE Face Roller + Face Oil above 2799",
+            "Additionally, Free Body Lotion on all orders above 899",
+            "Buy 2 Get 2 FREE on Bodycare Range",
+            "BUY 3 Get 25% of on Ikwi products",
+],
+        category:"Acne",
+       
+    },
+
+
+
+]
