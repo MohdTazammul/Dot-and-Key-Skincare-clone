b7affca7d43f15ec001b5632dc20ea88b0bda4d1
kultzz <93389594+kultzz@users.noreply.github.com>
2021-12-16 12:48:39 +0530

    making one css file
    including nav and footer css file in on folder (CSS)

---
+++ footer/footer.js
@@ -0,0 +1,45 @@
+function footer(){
+    return ` <div id="footBox">
+    <div id="footLeft">
+        <h3 class="footerTitle">GET TO KNOW US</h3>
+        <h3 class="footerContent">ABOUT US</h3>
+        <h3 class="footerContent">CONTACT US</h3>
+        <h3 class="footerContent">TERMS & CONDITION</h3>
+        <h3 class="footerContent">REFUND & CANCELLATION</h3>
+        <h3 class="footerContent">PRIVACY POLICY</h3>
+        <h3 class="footerContent">ORDER & SHIPPING</h3>
+        <h3 class="footerContent">AFFILIATE PROGRAM</h3>
+        <h3 class="footerContent">PAYMENT TERMS</h3>
+        <h3 class="footerContent">EARTHCARE</h3>
+        <h3 class="footerContent">TERMS OF SERVICE</h3>
+        <h3 class="footerContent">REFUND POLICY</h3>
+    </div>
+    
+    <div id="footCenter">
+        <h3 class="footerTitle">ALSO AVAILABLE ON</h3>
+        <h3 class="footerContent">NYKAA</h3>
+        <h3 class="footerContent">AMAZON</h3>
+        <h3 class="footerContent">FLIPKART</h3>
+        <h3 class="footerContent">MYNTRA</h3>
+        <h3 class="footerContent">FOXY</h3>
+    </div>
+    <div id="footRight">
+        <h3 class="footerTitle">STAY UPDATED</h3>
+        <div>
+            <input type="text" placeholder="example@example.com">
+            <button>SUBMIT</button>
+        </div>
+        
+        <div>
+            <h3>FOLLOW US</h3>
+            <i class="fab fa-facebook fa-2x"></i>
+            <i class="fab fa-instagram fa-2x"></i>
+            <i class="fab fa-youtube fa-2x"></i>
+
+        </div>
+        <div>© 2021 Dot & Key</div>
+    </div>
+   </div>`
+}
+
+export default footer
